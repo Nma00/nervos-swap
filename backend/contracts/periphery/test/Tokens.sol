b64113ee9bdddace7b99af470e585692e43bcfe9
Nicolas Manrique <nicolas.manrique@cnaf.fr>
2022-04-18 20:07:32 +0200

    Tests tokens

--- backend/contracts/periphery/test/Tokens.sol
+++ backend/contracts/periphery/test/Tokens.sol
@@ -4,14 +4,31 @@ pragma solidity =0.6.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenA is ERC20 {
-    constructor() public ERC20("Token A", "TOKENA") {
-        _mint(msg.sender, 100000 * 10**uint(decimals()));
+contract TokenTest is ERC20 {
+
+    uint128 constant INIT_SUPPLY = 100000;
+    
+    address private  _owner;
+
+    constructor(string memory name, string memory symbol) public ERC20(name, symbol) {
+        _owner = msg.sender;
+        _mint(msg.sender, INIT_SUPPLY * 10**uint(decimals()));
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == _owner);
+        _;
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount * 10**uint(decimals()));
     }
 }
 
-contract TokenB is ERC20 {
-    constructor() public ERC20("Token B", "TOKENB") {
-        _mint(msg.sender, 100000 * 10**uint(decimals()));
-    }
+contract TokenA is TokenTest {
+    constructor() public TokenTest("Token A", "TOKENA") {}
+}
+
+contract TokenB is TokenTest {
+    constructor() public TokenTest("Token B", "TOKENB") {}
 }
